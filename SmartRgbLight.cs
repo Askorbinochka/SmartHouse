449420a202c436fe3950dad88dbcb39724ee5fc3
Askorbinochka <tetyana.maletska@kpk-lp.com.ua>
2026-09-19 14:32:51 +0300

    create SmartRgbLight class

---
+++ SmartRgbLight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SmartHome;
+
+// Розумна підсвітка, яка змінює колір залежно від температури
+public class SmartRgbLighting : ISmartDevice
+{
+    public string Name => "RGB-Підсвітка «Atmosphere»";
+
+    public void OnClimateChanged(ClimateData climate)
+    {
+        if (climate.Temperature > 25)
+        {
+            Console.WriteLine($"  [{Name}]: Спекотно, вмикаю синє світло ");
+        }
+        else if (climate.Temperature < 18)
+        {
+            Console.WriteLine($"  [{Name}]: Прохолодно, вмикаю тепле помаранчеве ");
+        }
+        else
+        {
+            Console.WriteLine($"  [{Name}]: Температура хороша, свічу білим ");
+        }
+    }
+}
