6602a8aba977391ab917f79d13e0f48bf04f8258
Askorbinochka <tetyana.maletska@kpk-lp.com.ua>
2026-09-19 14:09:49 +0300

    create interface ClimateController

---
+++ IClimatController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SmartHome;
+
+public interface IClimateController
+{
+    //підключення пристрою
+    void Connect(ISmartDevice device);
+    //відключення пристрою
+    void Disconnect(ISmartDevice device);
+    //розсилати сповіщення
+    void NotifyDevices();
+}
