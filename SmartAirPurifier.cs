ab1fc025722155957c7e8fd97290f3d9837db253
Askorbinochka <tetyana.maletska@kpk-lp.com.ua>
2026-09-19 14:44:47 +0300

    create class SmartAirPurifier

---
+++ SmartAirPurifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace SmartHome;
+
+// Очищувач повітря, реагує на рівень пилу
+public class SmartAirPurifier : ISmartDevice
+{
+    public string Name => "Очищувач Повітря «PureAir»";
+
+    public void OnClimateChanged(ClimateData climate)
+    {
+        if (climate.AirQuality > 50)
+        {
+            Console.WriteLine($" [{Name}]: Багато пилу ( {climate.AirQuality}), вмикаю очищення! ");
+        }
+        else
+        {
+            Console.WriteLine($" [{Name}]: Повітря чисте, просто ароматизую лавандою ");
+        }
+    }
+}
